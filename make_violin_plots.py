5229f72b5445a94a11e7d307296e908c69077854
Logan Walker <loganaw@umich.edu>
2018-07-21 11:44:55 +0900

    Update make_violin_plots.py

--- make_violin_plots.py
+++ make_violin_plots.py
@@ -19,54 +19,59 @@ from glob import glob
 
 window_size = 250
 
-def dgaussian( x, amp, sig=1.0, cen=2.5 ):
-		out = exp( ( -1.0 * ( x - cen )**2.0 )/( 2.0 * sig**2.0 ) )
-		out += exp( ( -1.0 * ( x + cen - 10.0 )**2.0 )/( 2.0 * sig**2.0 ) )
-		out *= float( amp ) / float( 2 * sig * sqrt( 2 * pi ) )
-		return out
+def dgaussian( x, amp, sig=0.1, cen=0 ):
+	out = exp( ( -1.0 * ( x - cen )**2.0 )/( 2.0 * sig**2.0 ) )
+	out += exp( ( -1.0 * ( x + cen )**2.0 )/( 2.0 * sig**2.0 ) )
+	out *= float( amp ) / float( 2 * sig * sqrt( 2 * pi ) )
+	return out
 
 def load_file( name ):
-		out = []
+	out = []
 
-		for line in open( name, 'r' ):
-				if line.startswith( '#' ):
-						continue
+	for line in open( name, 'r' ):
+		if line.startswith( '#' ) or 'None' in line:
+			continue
 
-				line = line.strip().split( '\t' )
-				line[ 0 ] = int( line[ 0 ] )
-				line[ 2 ] = int( line[ 2 ] )
-				line[ 3 ] = float( line[ 3 ] )
+		line = line.strip().split( '\t' )
+		line[ 0 ] = float( line[ 0 ] )
+		line[ 1 ] = float( line[ 1 ] )
+		line[ 2 ] = int( line[ 2 ] )
+		line[ 4 ] = line[ 4 ].startswith( 'T' )
+	
+		# index, name, count, mu
+		#out.append( ( line[ 1 ], line[ 2 ], line[ 3 ] ) )
+		#33858.2075218	0.0817306656152	1489	CD74	False
+		out.append( ( line[ 3 ], line[ 0 ], line[ 1 ] ) )
+		del line
 
-				out.append( ( line[ 1 ], line[ 2 ], line[ 3 ] ) )
-				del line
-
-		return out
+	return out
 
 def find_state_change( dataset ):
-		x = []
-		scale = float( 2 * 10 )
-		for i in range( int( scale ) ):
-				x.append( i * 10.0 / scale + 0.5 / scale )
+	x = []
+	scale = float( 2 * 10 )
+	for i in range( int( scale ) ):
+		x.append( i * 10.0 / scale + 0.5 / scale )
 
-		for start in range( len( dataset ) / 10 ):
-				start *= 10
+	for start in range( len( dataset ) / 10 ):
+		start *= 10
+	
+		a = []
+		for i in range( window_size ):
+			a.append( dataset[ i + start ][ 2 ] )
 
-				a = []
-				for i in range( window_size ):
-						a.append( dataset[ i + start ][ 2 ] )
+		scale = int( scale )
+		bins = [ 0 ] * scale
+		for i in a:
+			bins[ int( i * scale ) ] += 1
 
-				scale = int( scale )
-				bins = [ 0 ] * scale
-				for i in a:
-						bins[ int( i * scale ) ] += 1
+		model = Model( dgaussian )
+		model.set_param_hint( 'cen', min=0.0, max=1.0 )
+		res = model.fit( bins, x=x, amp=window_size )
 
-				model = Model( dgaussian )
-				model.set_param_hint( 'cen', min=0.0, max=5.0 )
-				res = model.fit( bins, x=x, amp=window_size )
-
-				cutoff = 2.1
-				if res.params[ 'cen' ].value < cutoff:
-						return start
+		cutoff = 0.5
+#		cutoff = 2.5
+		if res.params[ 'cen' ].value > cutoff:
+			return start
 
 #########################################
 
