b59982b59b5ce2f06973d64993d5bd0ed70759d3
Logan Walker <loganaw@umich.edu>
2020-01-20 23:07:42 -0500

    Adding example files

--- example/run_example.sh
+++ example/run_example.sh
@@ -8,9 +8,5 @@
 
 for i in *.bed;
 do
-  python2 ../make_dat.py ncbiRefSeq.txt $i # generates `dat` files
-  python2 ../fitter.py $i.dat > $i.dat.txt # finds passing transcripts
+  echo "python2 ../make_dat.py ncbiRefSeq.txt $i; python2 ../fitter.py $i.dat > $i.dat.txt"
 done
-
-python2 ../grouper.py *.dat.txt > CLEAR_passed.txt # generates passed gene list
-python2 ../make_violin_plots.py # generates CLEAR_violins.pdf
