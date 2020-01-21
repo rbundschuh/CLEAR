8c2ee0cf528defab113d6adb132de13d811f397e
Logan Walker <loganaw@umich.edu>
2020-01-21 08:48:58 -0500

    Update example further

--- example/run_example.sh
+++ example/run_example.sh
@@ -6,11 +6,11 @@
 # Assumes that the bed files have already been generated and are in current
 # working directory
 
-for i in *.bed;
-do
-  python2 ../make_dat.py ncbiRefSeq.txt $i # generates `dat` files
-  python2 ../fitter.py $i.dat > $i.dat.txt # finds passing transcripts
-done
+#for i in *.bed;
+#do
+#  python2 ../make_dat.py ncbiRefSeq.txt $i # generates `dat` files
+#  python2 ../fitter.py $i.dat > $i.dat.txt # finds passing transcripts
+#done
 
-python2 ../grouper.py *.dat.txt > CLEAR_passed.txt # generates passed gene list
+python2 ../grouper.py --require-samples 3 *.dat.txt > CLEAR_passed.txt # generates passed gene list
 python2 ../make_violin_plots.py # generates CLEAR_violins.pdf
